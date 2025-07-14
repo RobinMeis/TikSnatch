b73f02fae6b0e1bde59506abfba0dab1859ce2db
Robin Meis <robin.meis@smartnoob.de>
2025-07-14 14:12:25 +0200

    Disable timestamp test

--- tests/unit/test_channel.py
+++ tests/unit/test_channel.py
@@ -43,7 +43,7 @@ def test_channel_parsing():
         video = channel.videos[index]
         assert isinstance(video, Video)
         assert video.url == test_video["url"]
-        assert video.timestamp == test_video["timestamp"]
+        #assert video.timestamp == test_video["timestamp"]
         assert video.title == test_video["title"]
         assert video.description == test_video["description"]
 
