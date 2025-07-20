5014a8a24ace96945839c2d11949ccb0cfc1e934
Robin Meis <robin.meis@smartnoob.de>
2025-07-20 15:22:52 +0200

    Formate date

--- TikSnatch/video.py
+++ TikSnatch/video.py
@@ -31,7 +31,7 @@ class Video:
 
     def download(self, output_directory):
         """Download a TikTok video using yt-dlp."""
-        timestamp = self.timestamp.strftime("%Y%m%d_%H%M%S")
+        timestamp = self.timestamp.strftime("%Y-%m-%d_%H%M%S")
         file_name = f"{timestamp}_{self.channel.username}_{self.id}.mp4"
         self.filepath = path.join(output_directory, file_name)
         
